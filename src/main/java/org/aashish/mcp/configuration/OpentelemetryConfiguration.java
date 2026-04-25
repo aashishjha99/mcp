77924a9af693cff86a41508c84a49ed7e62dbf7a
Aashish <60545932+aashishjha99@users.noreply.github.com>
2026-04-26 03:08:37 +0530

    fix: disable custom OTel config to allow Spring Boot auto-configuration to work

--- src/main/java/org/aashish/mcp/configuration/OpentelemetryConfiguration.java
+++ src/main/java/org/aashish/mcp/configuration/OpentelemetryConfiguration.java
@@ -5,11 +5,15 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+/**
+ * Disabling custom configuration to allow Spring Boot 3.3 auto-configuration 
+ * to take over using properties defined in application.yaml.
+ */
+// @Configuration
 public class OpentelemetryConfiguration {
-  @Bean
+  // @Bean
   public OtlpGrpcSpanExporter otlpGrpcSpanExporter(
-      @Value("${opentelemetry.exporter.otlp.endpoint}") String url) {
+      @Value("${management.otlp.tracing.endpoint:http://localhost:4317}") String url) {
     return OtlpGrpcSpanExporter.builder().setEndpoint(url).build();
   }
 }
