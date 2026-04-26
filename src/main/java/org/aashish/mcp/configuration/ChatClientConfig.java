d5435fa4a7fd922bf2659a7dcf2bc725111084e5
Aashish <60545932+aashishjha99@users.noreply.github.com>
2026-04-26 05:32:51 +0530

    fix: use ChatClient.Builder to ensure observations and metrics are properly enabled

--- src/main/java/org/aashish/mcp/configuration/ChatClientConfig.java
+++ src/main/java/org/aashish/mcp/configuration/ChatClientConfig.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.aashish.mcp.advisor.TokenUsageAuditAdvisor;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
-import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -13,10 +12,10 @@ import org.springframework.context.annotation.Primary;
 @Configuration
 public class ChatClientConfig {
 
-  @Bean()
+  @Bean
   @Primary
-  public ChatClient openAiChatClient(OpenAiChatModel openAiChatModeln, MeterRegistry meterRegistry) {
-    return ChatClient.builder(openAiChatModeln)
+  public ChatClient openAiChatClient(ChatClient.Builder builder, MeterRegistry meterRegistry) {
+    return builder
         .defaultAdvisors(List.of(new SimpleLoggerAdvisor(), new TokenUsageAuditAdvisor(meterRegistry)))
         .build();
   }
